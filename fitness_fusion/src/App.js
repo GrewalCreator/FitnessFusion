5bae25e02404d1dd5fc8724f273902f5485566bf
Gurtej Grewal <gurtejgrewal455@gmail.com>
2024-03-20 12:48:45 -0400

    Setup basicreact frontend with flask backend connection

---
+++ fitness_fusion/src/App.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react'
+
+function App(){
+
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    fetch("/test").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+  return (
+    <div>
+      {(typeof data.object === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        data.object.map((o, i) => (
+          <p key={i}>{o}</p>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default App
